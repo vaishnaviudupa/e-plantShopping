d260ccea019c25ee6d2dc19e409128224e60019c
vaishnaviudupa <vaishnaviudupa@gmail.com>
2026-06-18 06:15:56 -0400

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import { Provider } from 'react-redux'   //This component enables all components in the application to access the Redux store.
+import store from './store.js'           //This store holds the application's state, using the reducer defined in the CartSlice.jsx file.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/*The App component is wrapped with the Provider component, with the Redux store passed as a prop. This allows all components in the app to access and interact with the global state managed by Redux. */}
     <Provider store={store}>
     <App />
     </Provider>
